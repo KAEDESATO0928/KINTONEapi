1cf1c9c61139449d1d16853cdeb911531c4e09f3
KAEDESATO0928 <kaede.sato@apclo.net>
2025-08-30 22:14:13 +0900

    WiMAX項目非表示設定ver.5

--- WiMAX項目非表示設定.js
+++ WiMAX項目非表示設定.js
@@ -1,14 +1,14 @@
 (function() {
     'use strict';
     
-    // ユーザーID
-    const targetUserID = 'mikito';
+    // ユーザーName
+    const targetUserName = '株式会社樹人';
 
     kintone.events.on(['app.record.detail.show', 'app.record.edit.show'], function(event) {
         
         const userInfo = kintone.getLoginUser();
 
-        if (userInfo.loginName === targetUserID) {
+        if (userInfo.loginName === targetUserName) {
             
             // --- 1. グループ「group1」「group2」「group3」全体を非表示にする ---
             kintone.app.record.setFieldShown('group1', false);
